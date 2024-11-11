612ae5a7d4abca0e875390e6d5a7023d0e118ff5
REDDY PRO <22135a0508@gvpce.ac.in>
2024-11-11 20:08:53 +0530

    Navbar updated

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,21 +1,27 @@
+"use client"
 import Link from "next/link"
+import { useRouter } from "next/router"
 import React from "react"
 
 export default function Navbar() {
+
+  const router= useRouter()
+  const path = router.pathname.split('/')[1]
+
   return (
     <div className="p-4 mx-auto flex justify-center items-center">
       <ul className="flex justify-between gap-5">
         <li>
-          <Link className="p-2 focus-within:text-sky-400" href={"/"}>Home</Link>
+          <Link className={`p-2  ${path==="/" && "focus-within:text-sky-400 underline"}`} href={"/"}>Home</Link>
         </li>
         <li>
-          <Link className="p-2 focus-within:text-sky-400" href={"/work"}>Work</Link>
+          <Link className={`p-2  ${path==="/work" && "focus-within:text-sky-400 underline"}`} href={"/work"}>Work</Link>
         </li>
         <li>
-          <Link className="p-2 focus-within:text-sky-400" href={"/profile"}>Profile</Link>
+          <Link className={`p-2  ${path==="/profile" && "focus-within:text-sky-400 underline"}`} href={"/profile"}>Profile</Link>
         </li>
         <li>
-          <Link className="p-2 focus-within:text-sky-400" href={"/auth/login"}>Login</Link>
+          <Link className={`p-2  ${path==="/login" && "focus-within:text-sky-400 underline"}`} href={"/auth/login"}>Login</Link>
         </li>
       </ul>
     </div>

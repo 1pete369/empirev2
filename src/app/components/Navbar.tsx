d1cbc7c1b6e68dbc2b097f2d5bdca388f31b4e6c
REDDY PRO <22135a0508@gvpce.ac.in>
2024-11-11 20:20:21 +0530

    Navbar completed

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,41 +1,31 @@
 "use client"
 import Link from "next/link"
-import { useRouter } from "next/router"
+import { usePathname } from "next/navigation" // Import usePathname from next/navigation
 import React, { useEffect, useState } from "react"
 
 export default function Navbar() {
   const [path, setPath] = useState<string | null>(null)
-  const [mounted, setMounted] = useState(false)  // State to track whether the component is mounted
-  const router = useRouter()
+  const pathname = usePathname() // Get the current path using usePathname
 
   useEffect(() => {
-    // Set mounted to true after the component mounts
-    setMounted(true)
-  }, [])
-
-  useEffect(() => {
-    if (mounted) {
-      const pathName = router.pathname.split('/')[1]
-      setPath(pathName)
-    }
-  }, [router.pathname, mounted])  // Run the effect when router pathname changes or after mounting
-
-  if (!mounted) return null  // Ensure no rendering before the component is mounted
+    const pathName = pathname.split('/')[1]  // Get the first segment of the path
+    setPath(pathName)
+  }, [pathname]) // Re-run the effect when pathname changes
 
   return (
     <div className="p-4 mx-auto flex justify-center items-center">
       <ul className="flex justify-between gap-5">
         <li>
-          <Link className={`p-2  ${path === "/" && "focus-within:text-sky-400 underline"}`} href={"/"}>Home</Link>
+          <Link className={`p-2  ${path === "" && "text-sky-400 underline"}`} href={"/"}>Home</Link>
         </li>
         <li>
-          <Link className={`p-2  ${path === "/work" && "focus-within:text-sky-400 underline"}`} href={"/work"}>Work</Link>
+          <Link className={`p-2  ${path === "work" && "text-sky-400 underline"}`} href={"/work"}>Work</Link>
         </li>
         <li>
-          <Link className={`p-2  ${path === "/profile" && "focus-within:text-sky-400 underline"}`} href={"/profile"}>Profile</Link>
+          <Link className={`p-2  ${path === "profile" && "text-sky-400 underline"}`} href={"/profile"}>Profile</Link>
         </li>
         <li>
-          <Link className={`p-2  ${path === "/login" && "focus-within:text-sky-400 underline"}`} href={"/auth/login"}>Login</Link>
+          <Link className={`p-2  ${path === "auth" && "text-sky-400 underline"}`} href={"/auth/login"}>Login</Link>
         </li>
       </ul>
     </div>

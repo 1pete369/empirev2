832083967bc93211a096ae3e26a50f731a8b08a3
REDDY PRO <22135a0508@gvpce.ac.in>
2024-11-12 19:59:16 +0530

    Updated admin page

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -41,7 +41,7 @@ export default function Page() {
       <p className=" text-xl ">Admin DashBoard</p>
       <Link href={"/profile"} className=" border-2 rounded-sm flex items-center font-semibold"><BiArrowBack size={25}/></Link>
       </div>
-    <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 p-4 gap-4">
+    <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-4 p-4 gap-4">
       {
         isLoading && <p className="text-xl">Loading...</p>
       }
